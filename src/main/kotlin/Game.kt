30606e53ceffdafaa006d2b79291825bdf80bfbf
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-04-02 15:24:43 +0100

    WIP - Pair, highest kicker wins

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,10 +1,20 @@
 fun decideWinner(river: List<Card>, players: List<Player>): List<Player> {
 
-    val playersWithPairs = players.filter { player -> player.hand.isPair() }
+    val playersWithPairs =
+        players.filter { it.hasPair(river) }
+                .map { player -> Pair(player, player.hand.getValueOfPair(river)) }
+
     if (playersWithPairs.count() > 0) {
-        return playersWithPairs
+        val highestValue =
+            playersWithPairs.maxBy { (_, value) -> value }!!.let { (_, value) -> value }
+        return playersWithPairs.filter { (_, value) -> value == highestValue }.map { it.first }
     }
 
+    return filterForHighestCard(river, players)
+}
+
+
+fun filterForHighestCard(river: List<Card>, players: List<Player>): List<Player> {
     val highestValue = players.flatMap { it.hand }.maxBy { it.type.value }!!.type.value // !! == :(
     return players.filter { player ->
         val values = player.hand.map { card -> card.type.value }
@@ -12,7 +22,24 @@ fun decideWinner(river: List<Card>, players: List<Player>): List<Player> {
     }
 }
 
-fun List<Card>.isPair() = this[0].type.value == this[1].type.value
+fun List<Card>.getValueOfPair(river: List<Card>): Int {
+    if (this[0].type.value == this[1].type.value) {
+        return this[0].type.value
+    }
+
+    return this.first { handCard ->
+        river.map { riverCard ->
+            riverCard.type.value
+        }
+            .contains(handCard.type.value)
+    }.type.value
+}
+
+
+fun isPair(card1: Card, card2: Card) = card1.type.value == card2.type.value
+
+fun Player.hasPair(river: List<Card>) =
+    (river + card2).any { isPair(card1, it) } || (river + card1).any { isPair(card2, it) }
 
 enum class Suit(name: String) {
     HEARTS("Hearts"),
@@ -22,19 +49,19 @@ enum class Suit(name: String) {
 }
 
 sealed class CardType(val name: String, val value: Int) {
-    object TWO: CardType("Two", 2)
-    object THREE: CardType("Three", 3)
-    object FOUR: CardType("Four", 4)
-    object FIVE: CardType("Five", 5)
-    object SIX: CardType("Six", 6)
-    object SEVEN: CardType("Seven", 7)
-    object EIGHT: CardType("Eight", 8)
-    object NINE: CardType("Nine", 9)
-    object TEN: CardType("Ten", 10)
-    object JACK: CardType("Jack", 11)
-    object QUEEN: CardType("Queen", 12)
-    object KING: CardType("King", 13)
-    object ACE: CardType("Ace", 14)
+    object TWO : CardType("Two", 2)
+    object THREE : CardType("Three", 3)
+    object FOUR : CardType("Four", 4)
+    object FIVE : CardType("Five", 5)
+    object SIX : CardType("Six", 6)
+    object SEVEN : CardType("Seven", 7)
+    object EIGHT : CardType("Eight", 8)
+    object NINE : CardType("Nine", 9)
+    object TEN : CardType("Ten", 10)
+    object JACK : CardType("Jack", 11)
+    object QUEEN : CardType("Queen", 12)
+    object KING : CardType("King", 13)
+    object ACE : CardType("Ace", 14)
 }
 
 data class Card(val type: CardType, val suit: Suit)
--- src/test/kotlin/GameKtTest.kt
+++ src/test/kotlin/GameKtTest.kt
@@ -17,6 +17,17 @@ internal class GameKtTest {
     }
 
     @Test
+    fun `if two players have equivalent hands then they win and others lose`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(THREE, CLUBS), Card(FOUR, SPADES), Card(FIVE, DIAMONDS), Card(SIX, HEARTS))
+        val player1 = Player("Andy", Card(SEVEN, CLUBS), Card(NINE, CLUBS))
+        val player2 = Player("Bob", Card(SEVEN, DIAMONDS), Card(EIGHT, DIAMONDS))
+        val player3 = Player("Cheryl", Card(SEVEN, SPADES), Card(NINE, SPADES))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player1, player3))
+    }
+
+    @Test
     fun `if all players score highest card, player with highest card wins`() {
         val river =
             listOf(Card(TWO, HEARTS), Card(TWO, CLUBS), Card(TWO, SPADES), Card(TWO, DIAMONDS), Card(THREE, HEARTS))
@@ -45,6 +56,61 @@ internal class GameKtTest {
         val player1 = Player("Andy", Card(FIVE, CLUBS), Card(FIVE, HEARTS))
         val player2 = Player("Bob", Card(FIVE, DIAMONDS), Card(SEVEN, DIAMONDS))
         val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(SIX, SPADES))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player1))
+    }
+
+    @Test
+    fun `player with higher pair beats lower pair,`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(TWO, CLUBS), Card(TWO, SPADES), Card(TWO, DIAMONDS), Card(TEN, HEARTS))
+        val player1 = Player("Andy", Card(FIVE, CLUBS), Card(FIVE, HEARTS))
+        val player2 = Player("Bob", Card(SIX, CLUBS), Card(SIX, DIAMONDS))
+        val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(SIX, SPADES))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player2))
+    }
+
+    @Test
+    fun `player with pair beats high value card,`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(TWO, CLUBS), Card(TWO, SPADES), Card(TEN, DIAMONDS), Card(EIGHT, HEARTS))
+        val player1 = Player("Andy", Card(EIGHT, CLUBS), Card(QUEEN, HEARTS))
+        val player2 = Player("Bob", Card(TEN, CLUBS), Card(JACK, DIAMONDS))
+        val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(SIX, SPADES))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player2))
+    }
+
+    @Test
+    fun `player with pair from river beats high card,`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(FOUR, CLUBS), Card(FOUR, SPADES), Card(FOUR, DIAMONDS), Card(TEN, HEARTS))
+        val player1 = Player("Andy", Card(FIVE, CLUBS), Card(TWO, SPADES))
+        val player2 = Player("Bob", Card(SIX, CLUBS), Card(SEVEN, DIAMONDS))
+        val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(SIX, SPADES))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player1))
+    }
+
+    @Test
+    fun `players with pair, and equivalent kickers from river beats high card,`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(FOUR, CLUBS), Card(FOUR, SPADES), Card(FOUR, DIAMONDS), Card(TEN, HEARTS))
+        val player1 = Player("Andy", Card(FIVE, CLUBS), Card(TWO, SPADES))
+        val player2 = Player("Bob", Card(SIX, CLUBS), Card(SEVEN, DIAMONDS))
+        val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(TWO, CLUBS))
+
+        assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player1, player3))
+    }
+
+    @Test
+    fun `player with pair and highest kicker beats  player with same pair and lower kicker,`() {
+        val river =
+            listOf(Card(TWO, HEARTS), Card(FOUR, CLUBS), Card(FOUR, SPADES), Card(FOUR, DIAMONDS), Card(TEN, HEARTS))
+        val player1 = Player("Andy", Card(SIX, CLUBS), Card(TWO, SPADES))
+        val player2 = Player("Bob", Card(SIX, CLUBS), Card(SEVEN, DIAMONDS))
+        val player3 = Player("Cheryl", Card(FIVE, SPADES), Card(TWO, CLUBS))
 
         assertThat(decideWinner(river, listOf(player1, player2, player3))).isEqualTo(listOf(player1))
     }
